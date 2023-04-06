3f415624e549b2f148f6a65b23ef945254fe5994
Vava41 <120102035+Vava41@users.noreply.github.com>
2023-04-06 22:03:55 +0200

    final version

--- Assets/scripts/hello.cs
+++ Assets/scripts/hello.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 public class hello : MonoBehaviour
 {
    public AudioSource _audio;
-   public AudioClip clip;
+   public AudioClip aclip;
+
+   void Start()
+   {
+        _audio.clip= aclip;
+   }
+   
+   void Update()
+   {
+
+   }
+
    private void OnTriggerEnter(Collider other)
    {
-        _audio.PlayOneShot(clip);
+        _audio.Play();
    }
 }
